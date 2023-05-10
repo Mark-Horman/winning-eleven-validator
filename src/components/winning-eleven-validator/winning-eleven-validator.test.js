2360c4b3b75044150f212e273b1d83848cf76365
Mark Horman <mah8473@gmail.com>
2023-05-10 13:14:58 +1000

    corrected failing tests

--- src/components/winning-eleven-validator/winning-eleven-validator.test.js
+++ src/components/winning-eleven-validator/winning-eleven-validator.test.js
@@ -1,52 +1,61 @@
+// WinningElevenValidator.test.js
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect';
-import WinningElevenValidator from './WinningElevenValidator';
+import WinningElevenValidator from './winning-eleven-validator';
 
-describe('<WinningElevenValidator />', () => {
-  test('it should mount', () => {
-    render(<WinningElevenValidator />);
-    
-    const winningElevenValidator = screen.getByTestId('WinningElevenValidator');
-
-    expect(winningElevenValidator).toBeInTheDocument();
-  });
-});
-
-describe("validateObject function", () => {
-  it("should return null if object passes validation for topic A", () => {
-    const myObject = {
-      "topic": "A",
-      "name": "a",
-      "description": "something that is more than 10 and less than 100 characters"
+describe('WinningElevenValidator', () => {
+  it('renders an error message when name is incorrect for topic A', () => {
+    const obj = {
+      topic: 'A',
+      name: 'b',
+      description: 'This is a valid description.',
     };
-    expect(validateObject(myObject)).toBeNull();
+    render(<WinningElevenValidator ojectToValidate={obj} />);
+    const errorMessage = screen.getByText(/name must be 'a' for topic A/i);
+    expect(errorMessage).toBeInTheDocument();
   });
 
-  it("should return an error message if object fails validation for topic A", () => {
-    const myObject = {
-      "topic": "A",
-      "name": "b",
-      "description": "something that is too long and fails the validation"
+  it('renders an error message when description is too short for topic A', () => {
+    const obj = {
+      topic: 'A',
+      name: 'a',
+      description: 'Too short',
     };
-    expect(validateObject(myObject)).toBe("Name must be 'a' for topic A");
+    render(<WinningElevenValidator ojectToValidate={obj} />);
+    const errorMessage = screen.getByText(/description must be between 10 and 100 characters for topic A/i);
+    expect(errorMessage).toBeInTheDocument();
   });
 
-  it("should return null if object passes validation for topic B", () => {
-    const myObject = {
-      "topic": "B",
-      "name": "x",
-      "description": "short description"
+  it('renders an error message when name is incorrect for topic B', () => {
+    const obj = {
+      topic: 'B',
+      name: 'b',
+      description: 'This is a valid description.',
     };
-    expect(validateObject(myObject)).toBeNull();
+    render(<WinningElevenValidator ojectToValidate={obj} />);
+    const errorMessage = screen.getByText(/name must be 'x' for topic B/i);
+    expect(errorMessage).toBeInTheDocument();
   });
 
-  it("should return an error message if object fails validation for topic B", () => {
-    const myObject = {
-      "topic": "B",
-      "name": "y",
-      "description": "a description that is too long and fails the validation"
+  it('renders an error message when description is too long for topic B', () => {
+    const obj = {
+      topic: 'B',
+      name: 'x',
+      description: 'This description is too long. This description is too long.',
     };
-    expect(validateObject(myObject)).toBe("Name must be 'x' for topic B");
+    render(<WinningElevenValidator ojectToValidate={obj} />);
+    const errorMessage = screen.getByText(/description must be less than 40 characters for topic B/i);
+    expect(errorMessage).toBeInTheDocument();
+  });
+
+  it('renders a success message when the object is valid', () => {
+    const obj = {
+      topic: 'A',
+      name: 'a',
+      description: 'This description is just right.',
+    };
+    render(<WinningElevenValidator ojectToValidate={obj} />);
+    const successMessage = screen.getByText(/object is valid!/i);
+    expect(successMessage).toBeInTheDocument();
   });
 });
